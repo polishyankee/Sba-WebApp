69f1c61d4fb46898b854ee118df5cc6925da904e
Polishyankee <stasicakamil@hotmail.com>
2020-10-29 08:58:23 +0100

    add Thymeleaf, end the controller section

--- src/main/java/com/example/sbawebapp/controller/AuthorController.java
+++ src/main/java/com/example/sbawebapp/controller/AuthorController.java
@@ -1,6 +1,25 @@
 package com.example.sbawebapp.controller;
 
 
+import com.example.sbawebapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
 
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthrors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+
+    }
 }
